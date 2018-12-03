551d542fc85ee1e2c39212c2b83caa2dc2c562e8
Fuzayl <Fuzayl.Tuit@mail.ru>
2018-12-03 15:40:43 +0500

    working with device actors

---
+++ src/main/java/com/theaa/deviceActor/Device.java
@@ -0,0 +1,88 @@
+package com.theaa.deviceActor;
+
+import akka.actor.AbstractActor;
+import akka.actor.Props;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+import java.util.Optional;
+
+public class Device extends AbstractActor {
+
+    private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
+
+    final String groupId;
+    final String deviceId;
+
+    public Device(String groupId, String deviceId) {
+        this.groupId = groupId;
+        this.deviceId = deviceId;
+    }
+
+    public static Props props(String groupId, String deviceId) {
+        return Props.create(Device.class, () -> new Device(groupId, deviceId));
+    }
+
+    public static final class RecordTemperature {
+        final long requestId;
+        final double value;
+
+        public RecordTemperature(long requestId, double value) {
+            this.requestId = requestId;
+            this.value = value;
+        }
+    }
+
+    public static final class TemperatureRecorded {
+        final long requestId;
+
+        public TemperatureRecorded(long requestId) {
+            this.requestId = requestId;
+        }
+    }
+
+    public static final class ReadTemperature {
+        long requestId;
+
+        public ReadTemperature(long requestId) {
+            this.requestId = requestId;
+        }
+    }
+
+    public static final class RespondTemperature {
+        long requestId;
+        Optional<Double> value;
+
+        public RespondTemperature(long requestId, Optional<Double> value) {
+            this.requestId = requestId;
+            this.value = value;
+        }
+    }
+
+    Optional<Double> lastTemperatureReading = Optional.empty();
+
+    @Override
+    public void preStart() throws Exception {
+        log.info("Device actor {}-{} started", groupId, deviceId);
+    }
+
+    @Override
+    public void postStop() throws Exception {
+        log.info("Device actor {}-{} stopped", groupId, deviceId);
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(RecordTemperature.class, r -> {
+                    log.info("Record temperature reading {} with {}", r.value, r.requestId);
+                    lastTemperatureReading = Optional.of(r.value);
+                    getSender().tell(new TemperatureRecorded(r.requestId), getSelf());
+                })
+                .match(ReadTemperature.class, r -> {
+                    log.info("Get Last temperature value {} with {}", r.requestId);
+                    getSender().tell(new RespondTemperature(r.requestId, lastTemperatureReading), getSelf());
+                })
+                .build();
+    }
+}
